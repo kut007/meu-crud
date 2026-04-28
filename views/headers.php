c7f828bfad4e7b464d014d2f5581abf34e95b90d
unknown <antonio.rita.fer@gmail.com>
2026-04-28 14:55:25 -0300

    Header feito

---
+++ views/headers.php
@@ -0,0 +1,14 @@
+<?php // views/header.php ?>
+<!doctype html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+<title>CRUD Tarefas</title>
+</head>
+<body>
+<h1 class="flex justify-center text-xl font-bold">CRUD de Tarefas (PHP + MySQL)</h1>
+<hr>
+</body>
+</html>
